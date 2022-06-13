004397d506ae2f565f2d1ea93ad6f26343478ccf
Xemorr <31805746+Xemorr@users.noreply.github.com>
2022-06-13 16:35:37 +0100

    Added TARGETED trigger

--- src/main/java/me/xemor/skillslibrary2/triggers/Triggers.java
+++ src/main/java/me/xemor/skillslibrary2/triggers/Triggers.java
@@ -203,6 +203,7 @@ public class Triggers implements Listener {
     public void onTarget(EntityTargetLivingEntityEvent e) {
         if (e.getTarget() != null) {
             e.setCancelled(handleSkills(Trigger.getTrigger("TARGET"), e.getEntity(), e.getTarget()));
+            e.setCancelled(handleSkills(Trigger.getTrigger("TARGETED"), e.getTarget(), e.getEntity()));
         }
     }
 
