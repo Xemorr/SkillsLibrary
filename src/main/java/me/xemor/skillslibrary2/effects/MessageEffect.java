acb8c01382a8b5c20ec90ec8e98b18d777748dfa
Sam0fc <sammyhutton@gmail.com>
2022-07-19 13:08:43 -0700

    Switched from MineDown to MiniMessage

--- src/main/java/me/xemor/skillslibrary2/effects/MessageEffect.java
+++ src/main/java/me/xemor/skillslibrary2/effects/MessageEffect.java
@@ -1,9 +1,11 @@
 package me.xemor.skillslibrary2.effects;
 
-import de.themoep.minedown.adventure.MineDown;
 import me.xemor.skillslibrary2.SkillsLibrary;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.ParsingException;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -15,6 +17,12 @@ public class MessageEffect extends Effect implements EntityEffect, TargetEffect 
     public MessageEffect(int effect, ConfigurationSection configurationSection) {
         super(effect, configurationSection);
         this.message = configurationSection.getString("message");
+        try{
+            Component component = MiniMessage.miniMessage().deserialize(message, Placeholder.unparsed("player", ""));
+        }catch (ParsingException e) {
+            SkillsLibrary.getInstance().getLogger().severe("There is likely a legacy colour code at this location " + configurationSection.getCurrentPath() + ".message");
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -33,8 +41,13 @@ public class MessageEffect extends Effect implements EntityEffect, TargetEffect 
         if (entity instanceof Player) {
             Player player = (Player) entity;
             Audience audience = SkillsLibrary.getBukkitAudiences().player(player);
-            Component component = new MineDown(message).replaceFirst(true).replace("player", player.getName()).toComponent();
-            audience.sendMessage(component);
+            try {
+                Component component = MiniMessage.miniMessage().deserialize(message, Placeholder.unparsed("player", player.getDisplayName()));
+                audience.sendMessage(component);
+            }catch (ParsingException e) {
+                SkillsLibrary.getInstance().getLogger().severe("There is likely a legacy colour code in this message " + message);
+                e.printStackTrace();
+            }
         }
     }
 }
