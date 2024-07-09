e0738a57c6f789ad78d69685ef2ed1e03d6641c8
Sam (Xemor) <samuelhollis@outlook.com>
2024-07-09 16:38:10 +0100

    Register ParticlesLibrary

--- src/main/java/me/xemor/skillslibrary2/SkillsLibrary.java
+++ src/main/java/me/xemor/skillslibrary2/SkillsLibrary.java
@@ -1,5 +1,6 @@
 package me.xemor.skillslibrary2;
 
+import me.creeves.particleslibrary.ParticlesLibrary;
 import me.xemor.skillslibrary2.triggers.Triggers;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,6 +16,7 @@ public final class SkillsLibrary extends JavaPlugin {
         skillsLibrary = this;
         bukkitAudiences = BukkitAudiences.create(this);
         skillsManager = new SkillsManager();
+        ParticlesLibrary.registerParticlesLibrary(this);
         this.getServer().getPluginManager().registerEvents(new Triggers(), this);
     }
 
